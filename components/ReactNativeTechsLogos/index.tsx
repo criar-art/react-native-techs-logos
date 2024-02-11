2e6a392794bf82e1b6b26a6bd4ccb4a9bab7ff86
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-10 23:49:07 -0300

    add react-native-svg and example

--- components/ReactNativeTechsLogos/index.tsx
+++ components/ReactNativeTechsLogos/index.tsx
@@ -1,9 +1,14 @@
 import { Container, Label } from './styled';
 import { PropsType } from './types';
 
+import VueIcon from "./techs/VueIcon";
+import AndroidIcon from "./techs/AndroidIcon";
+
 export default (props: PropsType) => {
   return (
     <Container testID="techs-container">
+      <VueIcon width={120} height={40} />
+      <AndroidIcon width={120} height={40} />
       <Label>Teste teste</Label>
     </Container>
   );

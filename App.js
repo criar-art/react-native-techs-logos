b2a2d4e547a68a32128a5c98c04dd879d8854fbb
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-10 22:19:32 -0300

    initial

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>react-native-techs-logos</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

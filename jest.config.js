bb659a0eb744b88f533c32457b71f2c6ebb5b27d
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-11 05:26:15 -0300

    rewrite config babel and jest

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "@testing-library/react-native",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
-  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
-  snapshotFormat: {
-    printBasicPrototype: false,
-  },
-};

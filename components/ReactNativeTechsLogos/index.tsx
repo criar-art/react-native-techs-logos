7baadffbf98a33cd2499484a0fdfc8939cdb8325
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-11 09:41:37 -0300

    implementation flatlist in itens techs

--- components/ReactNativeTechsLogos/index.tsx
+++ components/ReactNativeTechsLogos/index.tsx
@@ -1,38 +1,27 @@
-import { FlatList, ScrollView } from 'react-native';
+import { FlatList } from 'react-native';
+
 import { ContainerTechs, ContentTech, Label } from './styled';
 import { PropsType } from './types';
-
 import techs from './techs';
 
 export default (props: PropsType) => {
+  const renderItem = ({ item }: any) => (
+    <ContentTech key={item.name}>
+      <item.icon width={120} height={40} />
+      <Label>{item.name}</Label>
+    </ContentTech>
+  );
+
   return (
     <ContainerTechs testID="techs-container">
-      <ScrollView contentContainerStyle={{
-        flexDirection: 'col',
-        flexWrap: 'wrap',
-      }}>
-        {/* <FlatList
-          data={techs}
-          renderItem={({ item }) => {
-            console.log('console.log(item):', item)
-            return (
-              <ContentTech key={item.name}>
-                <item.icon width={120} height={40} />
-                <Label>{item.name}</Label>
-              </ContentTech>
-            );
-          }}
-          keyExtractor={(item: any) => item.name}
-          showsHorizontalScrollIndicator={false}
-          numColumns={techs.length / 2}
-        /> */}
-        {techs.map((item: any, index: number) => (
-          <ContentTech key={index}>
-            <item.icon width={120} height={40} />
-            <Label>{item.name}</Label>
-          </ContentTech>
-        ))}
-      </ScrollView>
+      <FlatList
+        data={techs}
+        renderItem={renderItem}
+        horizontal={false}
+        keyExtractor={(item: any) => item.name}
+        showsHorizontalScrollIndicator={false}
+        numColumns={2}
+      />
     </ContainerTechs>
   );
 };

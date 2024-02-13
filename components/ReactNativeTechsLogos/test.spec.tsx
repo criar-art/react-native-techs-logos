eab4340d8922f06db064642b458a8f8a159f5232
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-13 00:10:02 -0300

    remove mock and adjustment for check first and second items list this change about flatlist render

--- components/ReactNativeTechsLogos/test.spec.tsx
+++ components/ReactNativeTechsLogos/test.spec.tsx
@@ -1,13 +1,9 @@
 import { render } from '@testing-library/react-native';
 import ReactNativeTechsLogos from '.';
-import { techs } from './mock';
 
-test('should render the label test', () => {
+test('should render first and second icon', () => {
   const { getByText } = render(<ReactNativeTechsLogos />);
 
-  for(let item of techs) {
-    const re = new RegExp(item.name, "i")
-    const label = getByText(re);
-    expect(label).toBeTruthy();
-  }
+  expect(getByText('Android')).toBeTruthy();
+  expect(getByText('Apple')).toBeTruthy();
 });

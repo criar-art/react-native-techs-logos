4fa2957a9c18c875091932640bb55988adf91452
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-12 23:59:35 -0300

    test(unit): improvement with mock test

--- components/ReactNativeTechsLogos/test.spec.tsx
+++ components/ReactNativeTechsLogos/test.spec.tsx
@@ -1,9 +1,13 @@
 import { render } from '@testing-library/react-native';
 import ReactNativeTechsLogos from '.';
+import { techs } from './mock';
 
 test('should render the label test', () => {
   const { getByText } = render(<ReactNativeTechsLogos />);
 
-  const label = getByText("Android");
-  expect(label).toBeTruthy();
+  for(let item of techs) {
+    const re = new RegExp(item.name, "i")
+    const label = getByText(re);
+    expect(label).toBeTruthy();
+  }
 });

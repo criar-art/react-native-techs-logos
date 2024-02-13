2234e8b94ee32e6290ce4d624da1950c9a560693
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-13 00:32:42 -0300

    implementation methos for render list or itens

--- components/ReactNativeTechsLogos/index.tsx
+++ components/ReactNativeTechsLogos/index.tsx
@@ -3,25 +3,49 @@ import { PropsType } from './types';
 import techs from './techs';
 
 export default (props: PropsType) => {
+  const getTech = (name: string) => {
+    return techs.find(item => item.name.toLowerCase() == name.toLowerCase())
+  }
+
+  const getTechs = (items: any) => techs.filter((tech => items.find((item: any) => {
+    return tech.name.toLowerCase() == item.toLowerCase()
+  })))
+
+  const hiddenTechs = (items: any) => techs.filter(item => !items.includes(item.name.toLocaleLowerCase()))
+
+  const renderList = () => {
+    if (props.list) {
+      return getTechs(props.list)
+    } else if (props.hiddenLogos) {
+      return hiddenTechs(props.hiddenLogos)
+    } else {
+      return techs
+    }
+  }
+
   const renderItem = ({ item }: any) => (
     <ContentTech key={item.name}>
       <item.icon width={120} height={40} />
-      <Label>{item.name}</Label>
+      {!props.hiddenLabel && <Label>{item.name}</Label>}
     </ContentTech>
   );
 
   return (
-    <ContainerTechs testID="techs-container">
-      <ListTechs
-        data={techs}
-        renderItem={renderItem}
-        horizontal={false}
-        keyExtractor={(item: any) => item.name}
-        showsHorizontalScrollIndicator={false}
-        numColumns={2}
-        contentContainerStyle={{ gap: 10 }}
-        columnWrapperStyle={{ gap: 10 }}
-      />
+    <ContainerTechs testID={`${props.list ? 'techs-container-list' : props.hiddenLogos ? 'techs-container-hiddenLogos' : 'techs-container'}`}>
+      {(props.name && getTech(props.name) && !props.list) ? (
+        renderItem({ item: getTech(props.name) })
+      ) : (
+        <ListTechs
+          data={renderList()}
+          renderItem={renderItem}
+          horizontal={false}
+          keyExtractor={(item: any) => item.name}
+          showsHorizontalScrollIndicator={false}
+          numColumns={2}
+          contentContainerStyle={{ gap: 10 }}
+          columnWrapperStyle={{ gap: 10 }}
+        />
+      )}
     </ContainerTechs>
   );
 };

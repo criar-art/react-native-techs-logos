b59ffe8be0f4b3575b53c41a3712483e0b08a991
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-02-15 11:47:57 -0300

    test: spec for render props of component

--- src/__tests__/index.spec.tsx
+++ src/__tests__/index.spec.tsx
@@ -50,3 +50,47 @@ test('should render list hiddenLogos', () => {
 
   expect(getByTestId('techs-container-hiddenLogos')).toBeTruthy();
 });
+
+test('should render props of component single', () => {
+  const { toJSON } = render(
+    <ReactNativeTechsLogos name="jest" background="#f00" size={200} rounded />
+  );
+
+  const { props, children } = toJSON().children[0];
+
+  expect(children[1].children[0]).toBe('Jest');
+  expect(props.background).toBe('#f00');
+  expect(props.style.backgroundColor).toBe('#f00');
+  expect(props.size).toBe(200);
+  expect(props.style.width).toBe(200);
+  expect(props.style.height).toBe(200);
+  expect(props.style.borderTopLeftRadius).toBe(100);
+  expect(props.style.borderTopRightRadius).toBe(100);
+  expect(props.style.borderBottomLeftRadius).toBe(100);
+  expect(props.style.borderBottomRightRadius).toBe(100);
+});
+
+test('should render props of component list', () => {
+  const { toJSON } = render(
+    <ReactNativeTechsLogos
+      list={['react', 'jest']}
+      background="#000"
+      size={150}
+      gap={50}
+    />
+  );
+
+  const { props, children } = toJSON().children[0].children[0];
+
+  expect(children[1].children[0]).toBe('React');
+  expect(props.background).toBe('#000');
+  expect(props.style.backgroundColor).toBe('#000');
+  expect(props.size).toBe(150);
+  expect(props.style.width).toBe(150);
+  expect(props.style.height).toBe(150);
+  expect(props.gap).toBe(50);
+  expect(props.style.marginTop).toBe(50);
+  expect(props.style.marginLeft).toBe(50);
+  expect(props.style.marginRight).toBe(50);
+  expect(props.style.marginBottom).toBe(50);
+});
